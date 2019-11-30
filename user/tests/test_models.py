b33f8c0d6f420c01aa596d4eb2e962c07e349c77
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-11-30 01:05:36 -0300

    Normalize email

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -6,7 +6,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@coffeapi.com'
+        email = 'tyrone@coffeapi.com'
         password = 'password'
         user = get_user_model().objects.create_user(
             email=email,
@@ -15,3 +15,14 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'tyrone@COFFEAPI.com'
+        password = 'password'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email.lower())
