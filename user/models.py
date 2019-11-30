e8ceab0b3c8c9812d9f58747470b1a312469c44b
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-11-30 01:12:54 -0300

    Adjustments

--- user/models.py
+++ user/models.py
@@ -6,9 +6,11 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **kwargs):
+    def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=self.normalize_email(email), **kwargs)
+        if not email:
+            raise ValueError('Users must have an email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -24,7 +26,6 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
         return self.create_user(email, password, **extra_fields)
-
 
 
 class User(AbstractBaseUser, PermissionsMixin):
