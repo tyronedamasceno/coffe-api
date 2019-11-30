f0cc583d9d3030366ff0c91d01a1e591e9e539dd
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-11-30 01:01:47 -0300

    Implement create_superuser on custom user manager

--- user/models.py
+++ user/models.py
@@ -14,6 +14,18 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that uses email instead username"""
